6c612a4ed6cc2f75b20f14c7615da20e8ba004ef
SamTo83 <Sam2thanh@hotmail.co.uk>
2022-01-25 08:28:27 +0000

    readme commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# MorseCode Translator
+
+![Morse Code page](https://github.com/SamTo83/Morse-Code-Translator/blob/main/morseCode.PNG)
+
+## Tech used
+* HTML5
+* JavaScript
+* Sass
+* Unit Testing
+
+## About
+
+The project allows me to experience unit testing and how to implement outcomes before coding. 
+
+
+The Morse Code translator allows the user to type on the English section and it will translate the morse code on the other output.  
--- translator.js
+++ translator.js
@@ -28,67 +28,26 @@ const letterStr = (letters) => {
 		'z':'--..'
 }
 	const letterArr = letters.split("");
-	console.log(letterArr);
 	const morseCode = letterArr.map((letter) => alphabet[letter]);
-	const morseCodes = morseCode.join(" ");
+	const morseCodes = morseCode.join(" /");
 	return morseCodes;
-	
 };
 
-const codeStr = (mCode) => {
-	const morseCoding = {
-    ".-": "a",
-    "-...": "b",
-    "-.-.": "c",
-    "-..": "d",
-    ".": "e",
-    "..-.": "f",
-    "--.": "g",
-    "....": "h",
-    "..": "i",
-    ".---": "j",
-    "-.-": "k",
-    ".-..": "l",
-    "--": "m",
-    "-.": "n",
-    "---": "o",
-    ".--.": "p",
-    "--.-": "q",
-    ".-.": "r",
-    "...": "s",
-    "-": "t",
-    "..-": "u",
-    "...-": "v",
-    ".--": "w",
-    "-..-": "x",
-    "-.--": "y",
-    "--..": "z",
-}
-
-	const morseArr = mCode.split(" ");
-	const alphabetCharacters = morseArr.map((char) => morseCoding[char]);
-	const englishText = alphabetCharacters.join("");
-	return englishText;
-};
-
-const userInput = document.querySelector('#user__input');
+const userInput = document.querySelector('.user__input');
 const submit = document.querySelector('.submit');
-const translation = document.querySelector('#translation');
+const translation = document.querySelector('.translation');
 const clear = document.querySelector('.clear');
 
-const displayTranslation = (words) => {
-	if (userInput.value) {
-		words = userInput.value;
-		translation.innerHTML = letterStr(words);
-	} else if (translation.value) {
-		words = translation.value;
-		userInput.innerHTML = codeStr(words);
-	}
-};
-submit.addEventListener('click', displayTranslation);
+submit.addEventListener('click', () => {
+	words = userInput.value;
+	translation.innerHTML = letterStr(words);
+});
 
 clear.addEventListener('click', () => {
 	translation.value = "";
 	userInput.value = "";
-	clear()
-})
+});
+
+
+
+
